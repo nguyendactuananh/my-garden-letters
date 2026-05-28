fd9dce6c031dd957cf22e1e327b2c0bf3fe3dacf
nguyendactuananh <dactuananh09092004@gmail.com>
2026-05-28 23:58:14 +0700

    Thiết lập cấu trúc thư mục frontend, backend và tính năng đính kèm 3 ảnh

---
+++ backend/main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Server Vườn Thư đang khởi động...");
+        // Logic kết nối database và xử lý API sẽ nằm ở đây
+    }
+}
